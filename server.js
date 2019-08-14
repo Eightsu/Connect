aa379e8b9fc9eb829c0eee647fa15930ed6aba0c
Jamar Powell <twoyearsyoung@gmail.com>
2019-08-14 15:52:35 -0700

    refactor for deployment

--- server.js
+++ server.js
@@ -12,22 +12,23 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 // environment Variables
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 
-app.get('/', (req, res) => res.send('API RUNNING'));
+// app.get('/', (req, res) => res.send('API RUNNING'));
 
 // Define routes
-app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/users', require('./routes/api/users'));
 
 if (process.env.NODE_ENV === 'production') {
-  // Set static folder
+  // Set static folder //! The client folder
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
+    // ?  Go from current directory into client/build and load index.
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }

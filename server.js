b525b70c7eeb32bc46a4b67e658bbce0521094bc
Jamar Powell <jamarpowell.dev@gmail.com>
2020-05-08 07:14:28 -0700

    refresh

--- server.js
+++ server.js
@@ -11,11 +11,9 @@ connectDB();
 // Initialize Middleware
 app.use(express.json({ extended: false }));
 
-// environment Variables
-const PORT = process.env.PORT || 3000;
 
 
-// app.get('/', (req, res) => res.send('API RUNNING'));
+app.get('/', (req, res) => res.send('API RUNNING'));
 
 // Define routes
 app.use('/api/auth', require('./routes/api/auth'));
@@ -26,12 +24,14 @@ app.use('/api/users', require('./routes/api/users'));
 if (process.env.NODE_ENV === 'production') {
   // Set static folder //! The client folder
   app.use(express.static('client/build'));
-
+  
   app.get('*', (req, res) => {
     // ?  Go from current directory into client/build and load index.
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
+// environment Variables
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.info(` coming in hot @ ${PORT}`);
